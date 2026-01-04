cf1cfcb0312b565914e2543b28603786f81021b4
gusknx <guuhkx@gmail.com>
2026-01-03 21:19:04 -0300

    Apagando tudo

--- tests/test_testes.py
+++
@@ -1,9 +0,0 @@
-"""ta faltando uma doc aqui tbm"""
-from app.teste import testando_coisas
-
-def test_testando_coisas_saida(capfd):
-    """faltando docstring aqui"""
-    testando_coisas()
-    out = capfd.readouterr()
-    assert 'teste, encerrado.' in out
-    assert 'vez errada' in out
